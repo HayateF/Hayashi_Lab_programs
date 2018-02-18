0b298cab869745c2da9247c8a066a96c7726fee9
Naoaki <mw.hidl.sd.lsp.02@gmail.com>
2018-02-19 00:03:24 +0900

    parameters optimized

--- Programs/1D_SI_VelHx.py
+++ Programs/1D_SI_VelHx.py
@@ -33,8 +33,8 @@ theta_SH = -0.20	# spin Hall angle.
 #alpha_R = 0	# Rashba parameter
 #C_1 = 3.0e-06	# velocity-DMI conversion coefficient.
 C_1 = 0.0
-#C_2 = 5.0e-16
 C_2 = 1.5e-16
+#C_2 = 0.0
 #voltage = 25 # voltage. 25V.
 #rho_W = # resistivity of W. Ohm*m.
 #rho_Ta = # resistivity of Ta.
@@ -60,7 +60,7 @@ H_z = 0
 current = 0.4e+12
 
 H_x_start = -1000e+03 / (4 * pi)
-H_x_end = 1150e+03 / (4 * pi)
+H_x_end = 1050e+03 / (4 * pi)
 H_x_step = 100e+03 / (4 * pi)
 H_x_list = np.arange(H_x_start, H_x_end, H_x_step, dtype = np.float64)	# x-field. A/m.
 velocity_eff_p_updown = np.zeros(H_x_list.size) 
